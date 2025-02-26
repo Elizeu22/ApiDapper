07f2bb37642fb36449abd3c3b5d3ea28b31349f5
Elizeu22 <elizeu29.silva@gmail.com>
2025-02-26 15:29:00 -0300

    Adicionado cors

---
+++ CorretoraTest/Controllers/TestCorretoraApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App_Corretora.Data;
+using App_Corretora.Repository;
+using App_Corretora.Models;
+using App_Corretora.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+using Xunit;
+
+namespace CorretoraTest.Controllers
+{
+    public class TestCorretoraApp
+    {
+        private readonly CorretoraRepository _repository;
+         
+        public TestCorretoraApp(CorretoraRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [Fact]
+        public async Task listarCorretoras()
+        {
+            //arrange
+            var corretoras =  await _repository.listarCorretoras();
+
+           
+
+            ///Assert 
+           corretoras.Should().Equals(corretoras);
+
+        }
+
+     
+    }
+}
