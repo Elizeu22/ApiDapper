408e298b8e675e01f587dbeecce6f8bea6b4edd3
Elizeu22 <elizeu29.silva@gmail.com>
2025-03-21 11:17:49 -0300

    Implementado Xunit para teste de api

--- CorretoraTest/Controllers/TestCorretoraApp.cs
+++ CorretoraTest/Controllers/TestCorretoraApp.cs
@@ -15,6 +15,7 @@ using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
 
 
 
@@ -70,7 +71,7 @@ namespace CorretoraTest.Controllers
         public async Task DeletarCNpj()
         {
 
-            string cnpj = "65895888";
+            string cnpj = "0122365898989878455";
 
             await using var factory = new WebApplicationFactory<Program>();
             var client = factory.CreateClient();
@@ -85,6 +86,57 @@ namespace CorretoraTest.Controllers
 
 
         }
+
+
+
+       [Fact]
+        public async Task CadastrarCnpj()
+        {
+
+            var corretora = new Corretora() { Cnpj = "09054789", corretora = "teste", Logradouro="teste", Cep="02354899", NomeSocial="teste" };
+            var payloadCorretora = JsonConvert.SerializeObject(corretora);
+            var contentCorretora = new StringContent(payloadCorretora, Encoding.UTF8, "application/json");
+
+            await using var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.PostAsync ("corretores/Gravar", contentCorretora);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+
+
+        }
+
+
+
+        [Fact]
+        public async Task AtualizarCnpj()
+        {
+
+            var corretora = new Corretora() { Cnpj = "0568784541313", corretora = "teste", Logradouro = "teste", Cep = "05689777", NomeSocial = "teste" };
+            var payloadCorretora = JsonConvert.SerializeObject(corretora);
+            var contentCorretora = new StringContent(payloadCorretora, Encoding.UTF8, "application/json");
+
+            await using var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.PutAsync("corretores/Atualizar", contentCorretora);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+
+
+        }
+
+
+
 
 
 
